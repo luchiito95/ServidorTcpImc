aff24d0bd5a139362be89158de60cd0cff528826
Luis Fernando Salas <luissala@bancolombia.com.co>
2024-02-16 20:51:19 -0500

    Servidor TCP IMC

---
+++ src/luissalas/imc/Principal.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package luissalas.imc;
+
+import luissalas.imc.vistas.VentanaPrincipal;
+
+/**
+ *
+ * @author luisf
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        VentanaPrincipal v = new VentanaPrincipal();
+        v.setLocationRelativeTo(null);
+        v.setVisible(true);
+    }
+
+}
